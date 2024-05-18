57e73a27bf98ada2cc70b82b79a03518bf05b241
jrxed <pkormin06@mail.ru>
2024-05-18 15:48:21 +0300

    change imports

--- src/python/init_db.py
+++
@@ -1,9 +0,0 @@
-from init_app import db
-
-
-def init_db():
-    db.create_all()
-
-
-if __name__ == '__main__':
-    init_db()
--- src/python/main.py
+++ src/python/main.py
@@ -1,4 +1,4 @@
-from app import *
+from .app import *
 
 if __name__ == '__main__':
     application.run(debug=True, host="192.168.1.50", port=8000)
